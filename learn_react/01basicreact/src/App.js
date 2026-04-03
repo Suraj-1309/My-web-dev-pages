308fa8558a6f1728f24dba0659389a4eafa78a2b
Suraj-1309 <surajsinghch2055@gmail.com>
2026-04-03 18:51:13 +0530

    starting react just for fun

---
+++ learn_react/01basicreact/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+    <h1>
+      Chai aur code | Suraj how to setup Project
+    </h1>
+    </>
+  );
+}
+
+export default App;
---
+++ learn_react/01vitereact/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <h1>
+      Hello Chai aur React with vite  | Seven
+    </h1>
+  )
+}
+
+export default App
