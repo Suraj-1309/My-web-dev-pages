9ca533b41d01b9879f932f3c4ad6d1918d9cad42
Suraj-1309 <surajsinghch2055@gmail.com>
2026-04-08 15:26:19 +0530

    add 06 React Currency Converter

---
+++ learn_react/06CurrencyConvertor/src/App.jsx
@@ -0,0 +1,94 @@
+import { useState } from "react";
+import { InputBox } from "./components";
+import useCurrencyInfo from "./hooks/useCurrenecy";
+
+function App() {
+  const [amount, setAmount] = useState(0);
+  const [from, setFrom] = useState("usd");
+  const [to, setTo] = useState("inr");
+  const [convertedAmount, setConvertedAmout] = useState(0);
+
+  const currencyInfo = useCurrencyInfo(from);
+
+  const options = Object.keys(currencyInfo);
+
+  const swap = () => {
+    setFrom(to);
+    setTo(from);
+    setConvertedAmout(amount);
+    setAmount(convertedAmount);
+  };
+
+  const convert = () => {
+    setConvertedAmout(amount * currencyInfo[to]);
+  };
+
+  return (
+    <div
+      className="flex justify-center align-center bg-cover bg-no-repeat min-h-screen w-full flex items-center"
+      style={{
+        backgroundImage: `url('https://images.pexels.com/photos/1054289/pexels-photo-1054289.jpeg?cs=srgb&dl=background-background-image-clouds-1054289.jpg&fm=jpg')`,
+      }}
+    >
+      <div className="h-screen flex flex-wrap justify-center items-center basis-2/3">
+        <div className="w-full flex">
+          <div className="basis-1/3">
+            <img
+              src="https://media1.tenor.com/m/dp8XlbrZ-d4AAAAC/shino-wag.gif"
+              alt=""
+            />
+          </div>
+          <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
+            <form
+              onSubmit={(e) => {
+                e.preventDefault();
+                convert();
+              }}
+            >
+              <div className="w-full mb-1">
+                <InputBox
+                  label="From"
+                  amount={amount}
+                  currencyOptions={options}
+                  onCurrencyChange={(currency) => setFrom(amount)}
+                  selectCurrency={from}
+                  onAmountChange={(amount) => setAmount(amount)}
+                />
+              </div>
+              <div className="relative w-full h-0.5">
+                <button
+                  type="button"
+                  className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"
+                  onClick={swap}
+                >
+                  swap
+                </button>
+              </div>
+              <div className="w-full mt-1 mb-4">
+                <InputBox
+                  label="To"
+                  amount={convertedAmount}
+                  currencyOptions={options}
+                  onCurrencyChange={(currency) => setTo(currency)}
+                  selectCurrency={to}
+                  onAmountChange={(amount) => setAmount(amount)}
+                  amountDisable
+                />
+              </div>
+              <button
+                type="submit"
+                className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg"
+              >
+                Convert {from.toUpperCase()} to {to.toUpperCase()}
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
+// https://media1.tenor.com/m/dp8XlbrZ-d4AAAAC/shino-wag.gif
